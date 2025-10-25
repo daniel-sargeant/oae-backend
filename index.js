ca82e14d153e6e09b81d771a4ed207ed24ca2c9b
Daniel Sargeant <daniel.sargeant@thebaltimorebanner.com>
2025-10-25 11:30:45 -0400

    add post & update

--- index.js
+++ index.js
@@ -1,28 +1,59 @@
-const express = require('express')
+const express = require("express");
 const { pool } = require("./client");
-const bodyParser = require('body-parser')
-const cors = require ('cors')
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
-)
+);
 
-app.get('/', (req, res) => {
-    pool.query('SELECT * FROM posts;', (error, results) => {
+app.get("/", (req, res) => {
+  pool.query("SELECT * FROM posts;", (error, results) => {
     if (error) {
-      throw error
+      throw error;
     }
-    res.status(200).json(results.rows)
-  })
-})
+    res.status(200).json(results.rows);
+  });
+});
+
+app.post("/", (req, res) => {
+  if (req.body.content?.length) {
+    const client = async () => await pool.connect();
+    if (req.body.id) {
+      client().then((x) => {
+        const transactionText = `UPDATE posts SET (content, updated_at) = ($1, NOW()) WHERE id = $2 RETURNING *`;
+        x.query(
+          transactionText,
+          [req.body.content, req.body.id],
+          (error, results) => {
+            if (error) {
+              throw error;
+            }
+            res.status(200).json(results.rows[0]);
+          }
+        );
+      });
+    } else {
+      client().then((x) => {
+        const transactionText = `INSERT INTO posts(content, created_at) VALUES ($1, NOW()) RETURNING *`;
+        x.query(transactionText, [req.body.content], (error, results) => {
+          if (error) {
+            throw error;
+          }
+          res.status(200).json(results.rows[0]);
+        });
+      });
+    }
+  }
+});
 
 app.listen(port, () => {
-  console.log(`App running on port ${port}.`)
-})
+  console.log(`App running on port ${port}.`);
+});
